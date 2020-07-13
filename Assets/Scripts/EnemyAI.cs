9a61fdffbafe3c6bc55b84136965a3d43b0a5c87
lshi <lshi@bu.edu>
2020-07-12 23:00:58 -0400

    added provoke to enemy AI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,6 +9,7 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] Transform target;
     [SerializeField] float chaseRange = 5f;
     NavMeshAgent navMeshAgent;
+    bool isProvoked;
     float distanceToTarget = Mathf.Infinity;
     
     // Start is called before the first frame update
@@ -20,10 +22,45 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         distanceToTarget = Vector3.Distance(target.position,transform.position);
-        if (distanceToTarget <= chaseRange)
+        if (isProvoked)
         {
+            //once engage, the enemy is not going to stop chasing the target
+            EngageWithTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
             navMeshAgent.SetDestination(target.position);
         }
         
+    }
+
+    private void EngageWithTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            chaseTarget();
+        }
+        else
+        {
+            attackTarget();
+        }
+    }
+
+    private void attackTarget()
+    {
+        Debug.Log(name + " is attacking " + target.name);
+    }
+
+    private void chaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
